030f153f9506305a8196bbf3af82f8fed0345dfa
informaton <hyattmoore@gmail.com>
2016-07-28 09:23:46 -0700

    #45 Resolved
    Correcting bug #45.  Had a misspelled variable name (‘ouput’)

--- PABatchTool.m
+++ PABatchTool.m
@@ -190,7 +190,7 @@ classdef PABatchTool < handle
         function didSet = setOutputPath(this,tmpOutputPath)
             if(~isempty(tmpOutputPath) && isdir(tmpOutputPath))
                 %assign the settings directory variable
-                this.settings.ouputDirectory = tmpOutputPath;
+                this.settings.outputDirectory = tmpOutputPath;
                 set(this.handles.text_outputPath,'string',tmpOutputPath);
                 this.updateOutputLogs();
                 didSet = true;
@@ -226,7 +226,6 @@ classdef PABatchTool < handle
            if(rawFileCount==0 && csvFileCount==0)
                msg = '0 files found.';
                set(this.handles.button_go,'enable','off');
-            
            else
               if(rawFileCount>0)
                   msg = sprintf('%u .raw file(s) found.\n',rawFileCount);
@@ -253,7 +252,7 @@ classdef PABatchTool < handle
            if(nargin<3)
                text_outputLogs_h = this.handles.text_outputLogs;
                if(nargin<2)
-                   outputPathname = this.settings.sourceDirectory;
+                   outputPathname = this.settings.outputDirectory;
                end
            end
           
