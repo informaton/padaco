e4cb7edc7bf190790d3ba6c35046322178366730
informaton <hyattmoore@gmail.com>
2018-07-05 11:30:45 -0700

    updates for rscript file locator

--- tools/getRScriptFile.m
+++ tools/getRScriptFile.m
@@ -1,27 +1,51 @@
+% Locates the R program to be used in executing R script files.
 function rscriptFile = getRScriptFile(file2check)
     curPath = mfilename('fullpath');
     settingsFile = fullfile(curPath,'rscript.path');
+    
     if(nargin<1 || isempty(file2check))
-        if(isunix)
-            file2check = '/usr/local/bin/Rscript';
-        else
-            file2check = 'Rscript.exe';
-        end
         if(exist(settingsFile,'file'))
             file2check = load(settingsFile,'-ascii');
+        else
+            file2check = '';
         end
     end
+
     if(exist(file2check,'file'))
         rscriptFile = file2check;
     else
-        if(isunix)
-            filterFile = 'Rscript';
+        if(ismac)
+            file2check = '/usr/local/bin/';
+            if(~exist(file2check,'file'))
+                file2check = '/Library/Frameworks/R.framework/Resources/Rscript';
+            end
+            
+        elseif(isunix)
+            file2check = '/usr/local/bin/Rscript';
+        elseif(ispc)
+            file2check = 'R_HOME/bin/Rscript.exe';
         else
-            filterFile = 'Rscript.exe';
+            file2check = 'Rscript*';  % not supported ...
         end
-        rscriptFile = uigetfullfile(filterFile,'Select ''RScript'' program which is required to run R scripts and is installed with a standard, separate, R installation.',file2check);        
-        if(exist(rscriptFile,'file'))
-            save(rscriptFile,'file2check','-ascii');
+        
+        if(exist(file2check,'file'))
+            rscriptFile = file2check;
+            try2save(rscriptFile);
+        else
+            
+            prompt = 'Select ''RScript'' program which is required to run R scripts and is installed with a standard, separate, R installation.';
+            uiwait(msgbox(prompt,'modal'));
+            rscriptFile = uigetfullfile(filterFile,prompt,file2check);
+            try2save(rscriptFile);
         end
+    end
+end
+
+function didSave = try2save(rscriptFile)
+    if(exist(rscriptFile,'file'))
+        save(rscriptFile,'file2check','-ascii');
+        didSave = true;
+    else
+        didSave = false;
     end
 end
