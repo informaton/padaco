0ca2e4a1e1ad9f4d085ede4c27cc91a4d27ec83a
informaton <hyattmoore@gmail.com>
2022-04-25 09:22:02 -0700

    cluster object update

--- tools/getClusterObject.m
+++ tools/getClusterObject.m
@@ -45,7 +45,9 @@ function clusterObj = getClusterObject(signalFilename, settingsFilename, setting
     if nargin==3
         extraLabel = ' with overriding settings';
     end
-    fprintf(1, 'Calculating prediction strength from %s%s.\n', signalFilename, extraLabel);
+    
+    % fprintf(1, 'Calculating prediction strength from %s%s.\n', signalFilename, extraLabel);
+    fprintf(1, 'Clustering from from %s%s.\n', signalFilename, extraLabel);
 
     clusterObj = PACluster(statTool.featureStruct.features,cSettings,[],[],statTool.featureStruct.studyIDs, statTool.featureStruct.startDaysOfWeek, delayedStart);
                 
